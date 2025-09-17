740bf8bfa720251bb1d04bb2c29bbdf3c6d464c1
SylvainZigoni <sylvain.zigoni@oclock.school>
2025-09-17 16:45:15 +0200

    gestion page detail promo ok

--- app/controllers/promo.controller.js
+++ app/controllers/promo.controller.js
@@ -1,46 +1,80 @@
 import promos from './../data/promos.json' with { type: 'json' };
 import students from './../data/students.json' with { type: 'json' };
 
+import client from '../db/client.js'
+
+
+// - FONCTIONS
+
+async function getPromos() {
+    const results = await client.query(`
+        SELECT *
+        FROM promo;`);
+    return results.rows;
+}
+
+async function getPromoById(pId){
+    console.log(pId);
+    const results = await client.query(`
+        SELECT *
+        FROM promo
+        WHERE id = ${pId};`);
+    return results.rows
+}
+
+async function getStudentByPromoId(pId){
+    const results = await client.query(`
+        SELECT *
+        FROM student
+        WHERE promo_id = ${pId};`);
+    return results.rows[0]
+}
+
+
+
+
 const promoController = {
-  listPromos(req, res) {
+  async listPromos(req, res) {
+    const promos =  await getPromos();
+    console.log(promos)
     res.render('promos', {
       // Je passe les données à la vue
       // Je vais pouvoir récupérer une variable `promos` dans ma vue `promos.ejs`
       promos, // écriture raccourcie de `promos: promos`
     });
   },
-  detailPromo(req, res) {
+
+  async detailPromo(req, res) {
     // Je suis sur une route paramétrée avec comme paramètre `:id` (voir dans promo.route.js)
     // Donc je peux récupérer l'id de la promo dans `req.params.id`
     const promoId = req.params.id;
     // Je vais chercher dans mon tableau de promos, celle avec l'id `promoId`
-    const promo = promos.find((promo) => promo.id === Number(promoId));
+    const promo = await getPromoById(Number(promoId));
 
     // Si je ne trouve pas de promo avec cet id, je renvoie une erreur 404
     if (!promo) {
       res.status(404).send('Cette promo n\'existe pas');
       return;
     }
-    
+    console.log(promo);
     res.render('promo', {
       promo
     });
   },
-  listStudents(req, res) {
+  async listStudents(req, res) {
     // Je vais devoir récupérer la promo correspondant à l'id dans l'URL 
     // ainsi que la liste des étudiants de cette promo
+    console.log(req.params.id);
     const promoId = req.params.id;
     // Je vais chercher dans mon tableau de promos, celle avec l'id `promoId`
-    const promo = promos.find((promo) => promo.id === Number(promoId));
+    const promo = await getPromoById(Number(promoId));
 
     if(!promo) {
       res.status(404).send('Cette promo n\'existe pas');
       return;
     }
 
-    const studentsFiltered = students.filter(
-      (student) => student.promo_id === Number(promoId)
-    );
+    const studentsFiltered = await getStudentByPromoId(Number(promoId));
 
     res.render('students', {
       promo,
--- docs/SQL/sql.md
+++ docs/SQL/sql.md
@@ -5,24 +5,53 @@
 ```sql
 -- toutes les promos, dans l'ordre alphabétique
 -- Je choisis les colonnes, ici toutes les colonnes avec *
-SELECT * 
+SELECT *
 -- Je précise la table
-FROM promo 
+FROM promo
 -- Je précise l'ordre, trier par nom de manière ascendante (ASC)
 ORDER BY name ASC;
 
 -- tous les étudiants, dans l'ordre alphabétique des noms de famille et des prénoms
-SELECT * 
-FROM student 
+SELECT *
+FROM student
 ORDER BY last_name ASC, first_name ASC;
 
 -- tous les étudiants de la promo 135
 SELECT *
-FROM student 
+FROM student
 WHERE promo_id = 135;
 
 -- les étudiants dont le nom ou le prénom ressemble à "max"
 SELECT *
 FROM student
 WHERE last_name ILIKE '%max%' OR first_name ILIKE '%max%';
+```
+
+-- Insérer dans la table "student" un étudiant qui s'appelle "Chuck Norris" appartenant à la promo 5
+
+```sql
+INSERT INTO student (first_name, last_name, promo_id)
+VALUES ('Chuck', 'Norris', 5);
+```
+
+-- Insérer dans la table "promo" une promo qui s'appelle "César" et ne possède pas d'organisation github
+
+```sql
+INSERT INTO promo (name)
+VALUES ('César');
+```
+
+-- Mettre a jour la promo 5 pour la renommer "Cleo"
+
+```sql
+UPDATE promo
+SET name = 'Cleo'
+WHERE id = 5;
+```
+
+-- Supprimer la promo 123
+
+```sql
+DELETE FROM promo
+WHERE id = 123;
 ```
