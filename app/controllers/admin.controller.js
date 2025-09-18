1934b4480ec467ec61036be30306de27f31816b8
SylvainZigoni <sylvain.zigoni@oclock.school>
2025-09-18 16:23:59 +0200

    avant challenge

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -6,13 +6,15 @@ const adminController = {
 	},
 
 	async addPromoSubmit(req, res) {
+		//_o ATTENTION IL MANQUE LA VERIFICATION DES DATA
+
 		const { name, github_organization } = req.body;
 		try {
 			const results = await adminMapper.addPromo(
 				name,
 				github_organization
 			);
-			res.render("promo_edit", { name, github_organization });
+			res.redirect("/promos");
 		} catch (error) {
 			console.error(`Erreur lors de l'ecriture des données`, error);
 			res.status(500).render("promo_edit");
